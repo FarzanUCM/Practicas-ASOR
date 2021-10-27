ebbcfa98594839723121a120e467db9f439cba03
RobertFarzan <robert.farzan@gmail.com>
2021-10-27 23:38:10 +0200

    Practica 2.1.b

--- practica2.1/ejercicio7.c
+++ practica2.1/ejercicio7.c
@@ -3,9 +3,8 @@
 #include <errno.h>
 
 int main() {
-   const char* curfile = "/home/usuario_vms/PracticaRobert/ejercicio7.c";
-   printf("Max number of links: %ld\n", pathconf(curfile, _PC_LINK_MAX));
-   printf("Max length of a filename: %ld\n", pathconf(curfile, _PC_NAME_MAX));
-   printf("Max length of a path: %ld\n", pathconf(curfile, _PC_PATH_MAX));
+   printf("Max number of links: %ld\n", pathconf("/bin/bash", _PC_LINK_MAX));
+   printf("Max length of a filename: %ld\n", pathconf("/bin/bash", _PC_NAME_MAX));
+   printf("Max length of a path: %ld\n", pathconf("/bin/bash", _PC_PATH_MAX));
    return 0;
 } 
